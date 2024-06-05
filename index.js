3b58b50f7a6f3316e75fb65f29bc594ebf97c5e9
Your Name <you@example.com>
2024-06-05 20:51:05 +0600

    added all

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kw08h9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 // afjolhossain020
 // bv5mUzBEfsgTEZZU
 
@@ -14,14 +16,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const client = new MongoClient(process.env.DATABASE_URL, {
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
 });
-
 async function run() {
   try {
     await client.connect();
